ae18322f9213a64f1d2ceb091d74e2877dbed92b
nqbui <43533648+nqbui@users.noreply.github.com>
2018-09-24 23:43:47 -0700

    Add files via upload

---
+++ ContinuousSend.py
@@ -0,0 +1,19 @@
+import socket
+import datetime
+import time
+ 
+def Main():
+        host = input("Enter IP to send to: ")
+        port = input("Enter Port to send to: ")
+        print("starting...")
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for x in range(0,60):
+                rightNow = datetime.datetime.now()
+                message="Time sent: " + datetime.datetime.now().strftime("%H:%M:%S.%f")
+                mySocket.sendto(message.encode(),(host,int(port)))       
+                time.sleep(.5) #wait .5 secs
+                 
+        mySocket.close()
+ 
+if __name__ == '__main__':
+    Main()
