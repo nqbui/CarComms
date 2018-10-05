6cec2ef4bf83f26976e493529a213e396ff7a396
nqbui <43533648+nqbui@users.noreply.github.com>
2018-10-05 01:02:02 -0700

    Add files via upload
    This script acts just like a server. It waits for a message then after receiving one it will send back a message.

---
+++ ReceiverSendBack.py
@@ -0,0 +1,23 @@
+#This script acts just like a server
+#It waits to receive a message then sends a message back
+import socket
+import datetime
+ 
+def Main():
+        listeningPort = input("Enter listening port: ")
+        host = input("Enter IP to send back to: ")
+        port = input("Enter Port to send back to: ")
+        print("starting...")
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        mySocket.bind(('0.0.0.0', int(listeningPort)))
+        data = ""
+        while data != 'q':
+            data = mySocket.recv(512).decode()
+            print("Received this:"+data)
+            message="Got your message: " + data
+            mySocket.sendto(message.encode(),(host,int(port)))       
+                 
+        mySocket.close()
+ 
+if __name__ == '__main__':
+    Main()
